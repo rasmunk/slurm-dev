de1c48d9ccf777994da4ec8aa00940f50bf16a51
Rasmus Munk <munk1@live.dk>
2019-01-21 01:19:47 +0100

    add slurm notebook config

--- jupyterhub/jupyterhub_config.py
+++ jupyterhub/jupyterhub_config.py
@@ -7,37 +7,37 @@ c.JupyterHub.spawner_class = 'jhub.SwarmSpawner'
 
 c.JupyterHub.ip = '0.0.0.0'
 c.JupyterHub.hub_ip = '0.0.0.0'
-
 c.JupyterHub.base_url = '/slurm'
+
+mounts = [{'type': 'bind',
+           'source': '/home/rasmus/repos/slurm-dev/config/slurm.conf',
+           'target': '/etc/slurm/slurm.conf'},
+          {'type': 'bind',
+           'source': '/home/rasmus/repos/slurm-dev/munge/munge.key',
+           'target': '/etc/munge/munge.key'}]
 
 # First pulls can be really slow, so let's give it a big timeout
 c.SwarmSpawner.start_timeout = 60 * 15
 
-c.SwarmSpawner.jupyterhub_service_name = 'slurm-dev'
+c.SwarmSpawner.jupyterhub_service_name = 'slurm-dev_jupyterhub'
 
-c.SwarmSpawner.networks = ["slurm-dev_default"]
+c.SwarmSpawner.networks = ["slurm-dev_slurm"]
 
 notebook_dir = os.environ.get('NOTEBOOK_DIR') or '/home/jovyan/work/'
 c.SwarmSpawner.notebook_dir = notebook_dir
 
-
 # 'args' is the command to run inside the service
 c.SwarmSpawner.container_spec = {
-    'args': ['/usr/local/bin/start-singleuser.sh',
-             '--NotebookApp.ip=0.0.0.0',
-             '--NotebookApp.port=8888'],
-    'env': {'JUPYTER_ENABLE_LAB': '1'}
+    'env': {'JUPYTER_ENABLE_LAB': '1',
+            'TZ': 'Europe/Copenhagen'}
 }
 
-# Before the user can select which image to spawn,
-# user_options has to be enabled
 c.SwarmSpawner.use_user_options = True
 
-# Available docker images the user can spawn
 c.SwarmSpawner.dockerimages = [
-    {'image': 'nielsbohr/slurm-notebook:latest',
-     'name': 'Base Image'
-     }
+    {'name': 'base-notebook',
+     'image': 'nielsbohr/slurm-notebook:edge',
+     'mounts': mounts}
 ]
 
 # Authenticator -> remote user header
@@ -46,4 +46,3 @@ c.DummyAuthenticator.password = 'password'
 
 # Pass the encoded username to the spawner
 c.Authenticator.enable_auth_state = False
-
