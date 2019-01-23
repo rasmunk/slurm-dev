849c8193de2775e07c602a4f72ff49aae24bbcea
Rasmus Munk <munk1@live.dk>
2019-01-23 02:14:21 +0100

    Remove build artifacts, update ignore

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/rasmus/repos/slurm-dev
-# Build directory: /home/rasmus/repos/slurm-dev/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- notebook/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jovyan/work/mpi
-# Build directory: /home/jovyan/work/mpi/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
